c40cd7e918086c22f47370b9b42139e92f345d41
Andrey Kondratov <andrew.kondratov@gmail.com>
2016-10-05 03:10:52 +0300

    Commit 2.

---
+++ CodehuntTests/CodehuntTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace Codehunt.Tests
+{
+    [TestFixture]
+    public class CodehuntTests
+    {
+        [TestCase(1, 0)]
+        [TestCase(2, 1)]
+        [TestCase(7, 91)]
+        [Test]
+        public void Level_02_05(int n, int result)
+        {
+            Assert.AreEqual(result, Methods.Level_02_05(n));
+        }
+
+        [TestCase("daaaaaaadddd", 'd', 5)]
+        [TestCase("paiibdeqqqqb", 'a', 1)]
+        [Test]
+        public void Level_02_07(string s, char x, int result)
+        {
+            Assert.AreEqual(result, Methods.Level_02_07(s, x));
+        }
+
+        [TestCase(1, 0, 1)]
+        [TestCase(1, 1, 1)]
+        [TestCase(2, 7, 128)]
+        [TestCase(-11, 9, 1937019605)]
+        [Test]
+        public void Level_03_01(int number, int power, int result)
+        {
+            Assert.AreEqual(result, Methods.Level_03_01(number, power));
+        }
+    }
+}
--- CodehuntTests/Properties/AssemblyInfo.cs
+++ CodehuntTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication9")]
+[assembly: AssemblyTitle("UnitTestProject2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication9")]
+[assembly: AssemblyProduct("UnitTestProject2")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("69d26ad2-fd55-457b-b686-d1d74f0fb40d")]
+[assembly: Guid("75da4f4e-ad0a-408d-a275-09ae25443b36")]
 
 // Version information for an assembly consists of the following four values:
 //
